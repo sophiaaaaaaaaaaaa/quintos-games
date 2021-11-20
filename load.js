e69c217d7bd2487bb8796c090d88719aa81e5012
sophiaaaaaaaaaaaa <sophiaz0505@gmail.com>
2021-11-21 11:13:14 +1300

    1.1.3

--- load.js
+++ load.js
@@ -1,3 +1,3 @@
 QuintOS.username = 'sophiaaaaaaaaaaaa'; // put your github username!
 QuintOS.dir = 'games_java'; // the folder with the game folder in it
-QuintOS.gameTitle = 'Pong'; // the title of the game to load
+QuintOS.gameTitle = 'Hangman'; // the title of the game to load
